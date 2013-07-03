b2736459bd4fac5854473166b232dd1c9bfd4745
niktrop <Nikolay.Tropin@jetbrains.com>
2013-07-03 11:53:34 +0400

    Bug with default settings fixed, redundant null checks removed #SCL-5844: Fixed (cherry picked from commit 42b29dc)

--- src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
+++ src/org/jetbrains/plugins/scala/settings/ScalaApplicationSettings.java
@@ -11,8 +11,6 @@ import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 
-import java.util.ArrayList;
-
 @State(
     name = "ScalaApplicationSettings",
     storages = {
@@ -24,10 +22,10 @@ import java.util.ArrayList;
 public class ScalaApplicationSettings implements PersistentStateComponent<ScalaApplicationSettings> {
 
   public String DEFAULT_SCALA_LIB_NAME = null;
-  public Boolean SPECIFY_TYPE_EXPLICITLY = null;
-  public Boolean INTRODUCE_LOCAL_CREATE_VARIABLE = null;
-  public Boolean SPECIFY_RETURN_TYPE_EXPLICITLY = null;
-  public Boolean INTRODUCE_PARAMETER_CREATE_DEFAULT = null;
+  public boolean SPECIFY_TYPE_EXPLICITLY = true;
+  public boolean INTRODUCE_LOCAL_CREATE_VARIABLE = false;
+  public boolean SPECIFY_RETURN_TYPE_EXPLICITLY = true;
+  public boolean INTRODUCE_PARAMETER_CREATE_DEFAULT = true;
   public boolean ADD_UNAMBIGUOUS_IMPORTS_ON_THE_FLY = false;
   public boolean SHOW_IMPORT_POPUP = true;
   public int ADD_IMPORTS_ON_PASTE = CodeInsightSettings.ASK;
